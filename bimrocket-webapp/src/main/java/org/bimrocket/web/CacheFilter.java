e0e08c6e17395c49c11ade899e23ee51c305bd8f
realor <realor@santfeliu.cat>
2021-08-17 20:50:53 +0200

    Add a web filter to control caching of js and css files.

---
+++ bimrocket-webapp/src/main/java/org/bimrocket/web/CacheFilter.java
@@ -0,0 +1,61 @@
+/*
+ * BIMROCKET
+ *
+ * Copyright (C) 2021, Ajuntament de Sant Feliu de Llobregat
+ *
+ * This program is licensed and may be used, modified and redistributed under
+ * the terms of the European Public License (EUPL), either version 1.1 or (at
+ * your option) any later version as soon as they are approved by the European
+ * Commission.
+ *
+ * Alternatively, you may redistribute and/or modify this program under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either  version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the licenses for the specific language governing permissions, limitations
+ * and more details.
+ *
+ * You should have received a copy of the EUPL1.1 and the LGPLv3 licenses along
+ * with this program; if not, you may find them at:
+ *
+ * https://joinup.ec.europa.eu/software/page/eupl/licence-eupl
+ * http://www.gnu.org/licenses/
+ * and
+ * https://www.gnu.org/licenses/lgpl.txt
+ */
+package org.bimrocket.web;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ *
+ * @author realor
+ */
+@WebFilter(urlPatterns = {"*.js", "*.css"})
+public class CacheFilter extends HttpFilter
+{
+  private static final long serialVersionUID = 1L;
+  private static final int SECONDS = 3600; // 1 hour
+
+  @Override
+  protected void doFilter(HttpServletRequest request,
+    HttpServletResponse response, FilterChain chain)
+    throws IOException, ServletException
+  {
+    response.setHeader("Cache-control","max-age=" + SECONDS);
+    long expires = System.currentTimeMillis() + SECONDS * 1000;
+		response.setDateHeader("Expires", expires);
+	  chain.doFilter(request, response);
+  }
+}

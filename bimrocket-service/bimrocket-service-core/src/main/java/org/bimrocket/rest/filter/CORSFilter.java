7c9b9a707572c8dcbb407dc2609ed1d5581fcc12
miguel-diaz-i2cat <miguel.diaz@i2cat.net>
2026-06-10 16:20:43 +0200

    Reset Cors Filter

--- bimrocket-service/bimrocket-service-core/src/main/java/org/bimrocket/rest/filter/CORSFilter.java
+++ bimrocket-service/bimrocket-service-core/src/main/java/org/bimrocket/rest/filter/CORSFilter.java
@@ -49,7 +49,7 @@ public class CORSFilter implements ContainerResponseFilter
     ContainerResponseContext responseContext) throws IOException
   {
     MultivaluedMap<String, Object> headers = responseContext.getHeaders();
-    headers.add("Access-Control-Allow-Origin", "http://localhost:8181");
+    headers.add("Access-Control-Allow-Origin", "*");
     headers.add("Access-Control-Allow-Credentials", "true");
     headers.add("Access-Control-Allow-Headers",
      "origin,content-type,accept,authorization,depth,if-modified-since,if-none-match,x-requested-with,destination");
